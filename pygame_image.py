79e88f27bd636d52ff758108d1f7e8cc9cc09889
C0B23011 <c0b230117c@edu.teu.ac.jp>
2024-11-19 15:55:54 +0900

    練習６ー背景画像が流れるようにする

--- pygame_image.py
+++ pygame_image.py
@@ -9,17 +9,22 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像surfaceを作成する
+    kk_img = pg.image.load("fig/3.png") #こうかとん画像surfaceを作成する
+    kk_img = pg.transform.flip(kk_img,True,False)
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
+  
 
 
 if __name__ == "__main__":
